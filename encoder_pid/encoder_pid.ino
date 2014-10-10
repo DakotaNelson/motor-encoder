b9d2c2e8e4d2e6b389e63df411063ff2a71169f5
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-10-10 12:26:18 -0400

    encoder read interrupt now works

--- encoder_pid/encoder_pid.ino
+++ encoder_pid/encoder_pid.ino
@@ -20,8 +20,9 @@ static double kd = 0.5;
 
 // starting position of the motor
 volatile byte ticks = 0; // ticks of the encoder - essentially position
-volatile unsigned int timeSum = 0; // Sum of the velocity queue. One less floating pont operation in the interrupt.
-volatile unsigned int tickTimes[5] = {0,0,0,0,0}; // time delta between ticks - used for velocity calculations
+volatile unsigned long timeSum = 0; // Sum of the velocity queue. One less floating pont operation in the interrupt.
+volatile unsigned long tickTimes[5] = {0,0,0,0,0}; // time delta between ticks - used for velocity calculations
+volatile unsigned long prevTime = 0;
 // 5 elements so that we have an average
 volatile byte timeIndex = 0; // where to insert into the tickTimes array
 
@@ -29,15 +30,18 @@ volatile byte timeIndex = 0; // where to insert into the tickTimes array
 boolean LED = false;
 
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(13, OUTPUT);
   digitalWrite(13,LOW);
-  MS.begin();
-  motor->setSpeed(128);
-  motor->run(FORWARD);
   
   attachInterrupt(0,readEncoder, CHANGE); // attaches to pin 2
   
-  motor->setSpeed(255);
+  MS.begin();
+  
+  delay(5000);
+  motor->setSpeed(110);
+  motor->run(FORWARD);
 }
 
 void loop() {
@@ -46,10 +50,17 @@ void loop() {
 
 void readEncoder() {
   timeSum -= tickTimes[timeIndex]; // subtract the oldest value in the array from running sum
-  int time = millis(); // get when the tick happened - useful for velocity
-  time -= tickTimes[(timeIndex + 4) % 5]; // get a time delta by subtracting the previous time
-  tickTimes[timeIndex] = time; // replace the oldest value with the new time delta
-  timeSum += time; // add the new value to the running sum
+  unsigned long time = millis(); // get when the tick happened - useful for velocity
+//  Serial.print("Raw time: ");
+//  Serial.println(time);
+  //time -= tickTimes[(timeIndex + 4) % 5]; // get a time delta by subtracting the previous time
+  
+//  Serial.print("Time delta (ms): ");
+//  Serial.println(time - prevTime);
+  tickTimes[timeIndex] = time - prevTime; // replace the oldest value with the new time delta
+  timeSum += time - prevTime; // add the new value to the running sum
+  
+  prevTime = time;
   timeIndex = (timeIndex+1) % 5; // move up where to insert into the array
   
   ticks = (ticks+1) % ENCODER_TICKS; // basically position
@@ -57,10 +68,20 @@ void readEncoder() {
   // for debugging:
   digitalWrite(13, !LED);
   LED = !LED;
-//  Serial.print("Time delta (ms): ");
-//  Serial.println(time);
+  
 //  Serial.print("Position: ");
 //  Serial.println(ticks);
+//  Serial.print("Array: ");
+//  Serial.print(tickTimes[0]);
+//  Serial.print(",");
+//  Serial.print(tickTimes[1]);
+//  Serial.print(",");
+//  Serial.print(tickTimes[2]);
+//  Serial.print(",");
+//  Serial.print(tickTimes[3]);
+//  Serial.print(",");
+//  Serial.println(tickTimes[4]);
+  
 }
 
 double getVelocity() {
