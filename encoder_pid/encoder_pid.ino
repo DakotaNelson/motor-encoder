1da1d8f99a47641925088be4405d3b42d23754bd
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-10-10 00:45:40 -0400

    reads encoder ticks; calculates position and velocity

--- encoder_pid/encoder_pid.ino
+++ encoder_pid/encoder_pid.ino
@@ -4,8 +4,11 @@
 Adafruit_MotorShield MS = Adafruit_MotorShield();
 Adafruit_DCMotor *motor = MS.getMotor(1);
 
+// some constants
+const unsigned short ENCODER_TICKS = 40; // number of ticks in one revolution of the encoder
+const unsigned short DEGREES_TICK = 360 / ENCODER_TICKS;
 
-// actual P, I, and D values
+// actual P, I, and D values for the PID controller
 double p;
 double i;
 double d;
@@ -15,17 +18,55 @@ static double kp = 0.5;
 static double ki = 0.5;
 static double kd = 0.5;
 
-// motor speed - current, ideal, and the command speed to send to the motor shield
-int mspeed; // the speed (0-255) at which to run the motor
-double current; // current speed
-double goal; // ideal speed
+// starting position of the motor
+volatile byte ticks = 0; // ticks of the encoder - essentially position
+volatile unsigned int timeSum = 0; // Sum of the velocity queue. One less floating pont operation in the interrupt.
+volatile unsigned int tickTimes[5] = {0,0,0,0,0}; // time delta between ticks - used for velocity calculations
+// 5 elements so that we have an average
+volatile byte timeIndex = 0; // where to insert into the tickTimes array
+
+// other
+boolean LED = false;
 
 void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13,LOW);
   MS.begin();
   motor->setSpeed(128);
-  motor->run(BACKWARD);
+  motor->run(FORWARD);
+  
+  attachInterrupt(0,readEncoder, CHANGE); // attaches to pin 2
+  
+  motor->setSpeed(255);
 }
 
 void loop() {
-  //motor->setSpeed(mspeed);
+  //motor->setSpeed(something);
+}
+
+void readEncoder() {
+  timeSum -= tickTimes[timeIndex]; // subtract the oldest value in the array from running sum
+  int time = millis(); // get when the tick happened - useful for velocity
+  time -= tickTimes[(timeIndex + 4) % 5]; // get a time delta by subtracting the previous time
+  tickTimes[timeIndex] = time; // replace the oldest value with the new time delta
+  timeSum += time; // add the new value to the running sum
+  timeIndex = (timeIndex+1) % 5; // move up where to insert into the array
+  
+  ticks = (ticks+1) % ENCODER_TICKS; // basically position
+  
+  // for debugging:
+  digitalWrite(13, !LED);
+  LED = !LED;
+//  Serial.print("Time delta (ms): ");
+//  Serial.println(time);
+//  Serial.print("Position: ");
+//  Serial.println(ticks);
+}
+
+double getVelocity() {
+  return timeSum / 5; // average velocity over past 5 readings
+}
+
+double getPosition() {
+  return ticks * DEGREES_TICK; // what angle we're at, more or less
 }
